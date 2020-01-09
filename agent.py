29b7fad2ccb43e48fcbebdcf65f0d9bf61987838
lilybhattacharjee5 <lilybhattacharjeeca@gmail.com>
2020-01-09 11:50:33 -0800

    supports single-agent manhattan, distance heuristics

--- agent.py
+++ agent.py
@@ -1,14 +1,19 @@
 import math
 
+dirs = ["left", "right", "up", "down", "diag_ru", "diag_rd", "diag_lu", "diag_ld"]
+
 class Agent:
-    def __init__(self, start_x, start_y, size, canvas):
+    def __init__(self, start_x, start_y, dest_x, dest_y, size, canvas, color):
         self.x = start_x
         self.y = start_y
+        self.dest_x = dest_x
+        self.dest_y = dest_y
         self.size = size
         self.canvas = canvas
-        self.obj = canvas.create_rectangle(start_x, start_y, start_x + size, start_y + size, fill = 'blue', outline = 'blue')
+        self.obj = canvas.create_rectangle(start_x, start_y, start_x + size, start_y + size, fill = color, outline = color)
+        self.dist_traveled = 0
 
-    def move(self, mag_x, mag_y, dir):
+    def calc_move(self, mag_x, mag_y, direction):
         dir_x_y = {
             "left": (-mag_x, 0),
             "right": (mag_x, 0),
@@ -19,5 +24,37 @@ class Agent:
             "diag_lu": (-mag_x, -mag_y),
             "diag_ld": (-mag_x, mag_y),
         }
-        x_move, y_move = dir_x_y[dir]
+        return dir_x_y[direction]
+
+    def move(self, mag_x, mag_y, direction):
+        x_move, y_move = self.calc_move(mag_x, mag_y, direction)
+        self.x += x_move
+        self.y += y_move
+        self.dist_traveled += abs(x_move) + abs(y_move)
         self.canvas.move(self.obj, x_move, y_move)
+
+    def manhattan_heuristic(self, x, y):
+        x_remain = abs(self.dest_x - x)
+        y_remain = abs(self.dest_y - y)
+        return x_remain + y_remain
+
+    def distance_heuristic(self, x, y):
+        x_remain = self.dest_x - x
+        y_remain = self.dest_y - y
+        return (x_remain**2 + y_remain**2)**0.5
+
+    def next_move(self, mag_x, mag_y, heuristic):
+        min_cost = math.inf
+        opt_dir = "right"
+        for d in dirs:
+            x_offset, y_offset = self.calc_move(mag_x, mag_y, d)
+            next_x, next_y = self.x + x_offset, self.y + y_offset
+            next_heuristic = heuristic(next_x, next_y)
+            if next_heuristic + self.dist_traveled < min_cost:
+                min_cost = next_heuristic + self.dist_traveled
+                opt_dir = d
+        return opt_dir
+
+    def auto_move(self, mag_x, mag_y):
+        opt_dir = self.next_move(mag_x, mag_y, self.manhattan_heuristic)
+        self.move(mag_x, mag_y, opt_dir)
--- board.py
+++ board.py
@@ -3,8 +3,8 @@ import sys
 from agent import *
 
 cell_width = int(sys.argv[1])
-start_x = int(sys.argv[2])
-start_y = int(sys.argv[3])
+start_x, start_y = int(sys.argv[2]), int(sys.argv[3])
+end_x, end_y = int(sys.argv[4]), int(sys.argv[5])
 
 m = tk.Tk()
 
@@ -27,14 +27,34 @@ def config_grid(event = None):
     return create_grid(event, cell_width = cell_width)
 
 c = tk.Canvas(m, height = 1000, width = 1000, bg = 'black')
-agent = Agent(start_x, start_y, cell_width, c)
+
+header = tk.Frame(m)
+header_widget_h, header_widget_w = 1, 50
+
+pos = tk.Text(m, height = header_widget_h, width = header_widget_w)
+pos.insert(tk.INSERT, "Position: (%d, %d)" % (start_x, start_y))
+
+score = tk.Text(m, height = header_widget_h, width = header_widget_w)
+score.insert(tk.INSERT, "Score: ")
+
+pos.pack(in_ = header, side = 'left')
+score.pack(in_ = header, side = 'left')
+
+header.pack(side = 'top', anchor = 'w')
+
+destination = c.create_rectangle(end_x, end_y, end_x + cell_width, end_y + cell_width, fill = 'blue', outline = 'blue')
+agent = Agent(start_x, start_y, end_x, end_y, cell_width, c, 'yellow')
 
 c.pack(fill = tk.BOTH, expand = True)
 
 c.bind('<Configure>', config_grid)
 
 def move_agent():
-    agent.move(5, 5, "diag_lu")
-    c.after(100, move_agent)
+    if agent.x != end_x:
+        agent.auto_move(mag_x = cell_width, mag_y = cell_width)
+        pos.delete(1.0, tk.INSERT)
+        pos.insert(tk.INSERT, "Position: (%d, %d)" % (agent.x, agent.y))
+        c.after(100, move_agent)
+
 move_agent()
 m.mainloop()
